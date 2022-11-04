907157fcb98ffe7c1a71ab4628074bc05b9cbdad
Kendandra <NathanMRojas@gmail.com>
2022-11-03 22:52:43 -0400

    Cleaned up build script.

--- build.sh
+++ build.sh
@@ -14,26 +14,17 @@ echo Clearing build cache
 rm -v $BUILD_DIR/*
 echo ----------------------------
 
-echo Build css
+echo Build CSS
 # use default filesystem ordering as poormans CSS load order.  WOW.
 cat $SRC_DIR/*.css > $BUILD_DIR/skilltree.css
 echo ----------------------------
 
-echo Build copy
-# move all basic assets
+echo Build HTML
 cp -vp $SRC_DIR/*.html $BUILD_DIR
-#cp -vp $SRC_DIR/*.css $BUILD_DIR
 echo ----------------------------
 
-
-echo Wrapper replacement
-# build wrapper (crazy, I know)
-#sed -r '/\{\{REPLACEMET\}\}/e cat $SRC_HTML' ./$SRC_WRAPPER > $BUILD_DIR/$SRC_WRAPPER
-
-#sed -i "/REPLACEMET/r $SRC_WRAPPER" -e //N $SRC_HTML > $BUILD_DIR/$SRC_WRAPPER
-
+echo Build TEST HARNESS
 cat $SRC_WRAPPER_HEADER $SRC_HTML $SRC_WRAPPER_FOOTER > $BUILD_DIR/index.html
-
 echo ----------------------------
 
 echo BUILD SUCCESS???
